162a6ab1d787822091efb5712917ab7b0685264a
Liubastill <liubastill@gmail.com>
2022-07-13 12:04:47 +0300

    addaed 2 new themes

--- app/src/main/java/com/example/calculatorapp/model/Theme.java
+++ app/src/main/java/com/example/calculatorapp/model/Theme.java
@@ -1,25 +1,31 @@
 package com.example.calculatorapp.model;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.StringRes;
 import androidx.annotation.StyleRes;
 
 import com.example.calculatorapp.R;
 
 public enum Theme {
-    MAIN(R.style.Theme_CalculatorApp, R.string.main_theme, "thememain" ),
-    NIGHT(R.style.Theme_CalculatorApp_Night, R.string.night_theme, "themenight" ),
-    PURPLE(R.style.Theme_CalculatorApp_Purple, R.string.purple_theme, "themepurple" );
+    MAIN(R.style.Theme_CalculatorApp, R.string.main_theme, "thememain" , R.drawable.clouds ),
+    NIGHT(R.style.Theme_CalculatorApp_Night, R.string.night_theme, "themenight",  R.drawable.night_clouds),
+    PURPLE(R.style.Theme_CalculatorApp_Purple, R.string.purple_theme, "themepurple", R.drawable.purple_clouds ),
+    BLUE(R.style.Theme_CalculatorApp_Blue, R.string.blue_theme, "themeblue", R.drawable.blue_clouds ),
+    ORANGE(R.style.Theme_CalculatorApp_Orange, R.string.orange_theme, "themeorange", R.drawable.orange_clouds );
 
-    Theme(int themeRes, int title, String key) {
+    Theme(int themeRes, int title, String key, int picture) {
         this.themeRes = themeRes;
         this.title = title;
         this.key = key;
+        this.picture = picture;
     }
 
     @StyleRes
     private int themeRes;
     @StringRes
     private int title;
+    @DrawableRes
+    private int picture;
 
     private String key;
 
@@ -33,5 +39,9 @@ public enum Theme {
 
     public String getKey() {
         return key;
+    }
+
+    public int getPicture() {
+        return picture;
     }
 }
